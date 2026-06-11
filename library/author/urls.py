f594e53730578acb2471b049142cedce7ed064f2
Vira Nychkalo <nychkalovira@gmail.com>
2026-06-11 22:21:20 +0300

    updated author

--- library/author/urls.py
+++ library/author/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.author_list, name='author_list'),
+    path('create/', views.create_author, name='create_author'), # <--- ДОДАЙ ЦЕЙ РЯДОК
 ]
--- library/author/views.py
+++ library/author/views.py
@@ -3,5 +3,16 @@ from .models import Author
 
 def author_list(request):
     authors = Author.get_all() 
-
     return render(request, 'author/author_list.html', {'authors': authors})
+
+def create_author(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        surname = request.POST.get('surname')
+        patronymic = request.POST.get('patronymic')
+
+        if name and surname and patronymic:
+            Author.create(name=name, surname=surname, patronymic=patronymic)
+            return redirect('author_list')
+
+    return render(request, 'author/create_author.html')
