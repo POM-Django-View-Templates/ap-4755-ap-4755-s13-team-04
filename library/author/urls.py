a99836e597a833204e661a086caf0360b8131dd6
Vira Nychkalo <nychkalovira@gmail.com>
2026-06-11 23:09:59 +0300

    updated uathur

--- library/author/urls.py
+++ library/author/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.author_list, name='author_list'),
-    path('create/', views.create_author, name='create_author'), # <--- ДОДАЙ ЦЕЙ РЯДОК
+    path('create/', views.create_author, name='create_author'),
+    path('delete/<int:author_id>/', views.delete_author, name='delete_author'),
 ]
--- library/author/views.py
+++ library/author/views.py
@@ -14,5 +14,17 @@ def create_author(request):
         if name and surname and patronymic:
             Author.create(name=name, surname=surname, patronymic=patronymic)
             return redirect('author_list')
-
+        
     return render(request, 'author/create_author.html')
+
+def delete_author(request, author_id):
+    if request.method == 'POST':
+        author = Author.get_by_id(author_id)
+        if author:
+            # Перевіряємо, чи немає в автора книг
+            if not hasattr(author, 'book_set') or not author.book_set.exists():
+                Author.delete_by_id(author_id)
+            else:
+                print("У цього автора є книги, видаляти не можна!")
+                
+    return redirect('author_list')
