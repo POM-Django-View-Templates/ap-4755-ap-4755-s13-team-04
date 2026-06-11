1788a4f6834bcad63223654f365a6f646d49970c
Vira Nychkalo <nychkalovira@gmail.com>
2026-06-11 22:15:41 +0300

    updetad author

--- library/author/views.py
+++ library/author/views.py
@@ -1,1 +1,7 @@
-# Create your views here.
+from django.shortcuts import render, redirect
+from .models import Author
+
+def author_list(request):
+    authors = Author.get_all() 
+
+    return render(request, 'author/author_list.html', {'authors': authors})
